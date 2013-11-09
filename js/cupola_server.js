7025b6c9fb75b4eed96278ff8edbc1915d0947e5
Daniel Andersen <gadren@gmail.com>
2013-11-09 17:32:23 -0600

    regorganizing files

--- js/cupola_server.js
+++ js/cupola_server.js
@@ -1,4 +1,4 @@
-var UsbAdapter = function() {
+var CupolaServer = function() {
 	
 	var mPredictDt = 0.045;	// default is 0.03 seconds -- says how far to look-ahead when getting orientation
 													// higher values can offset lag, but can also lead to jerky acceleration/deceleration
@@ -6,11 +6,11 @@ var UsbAdapter = function() {
 	var mDebugEnabled = false;
 
 	if (!chrome.permissions) {
-		console.error("UsbAdapter: Requires chrome.permissions API");
+		console.error("CupolaServer: Requires chrome.permissions API");
 		return null;
 	}
 	if (!chrome.usb) {
-		console.error("UsbAdapter: Requires chrome.usb API");
+		console.error("CupolaServer: Requires chrome.usb API");
 		return null;
 	}
 
@@ -266,135 +266,3 @@ var UsbAdapter = function() {
 	};
 
 };
-
-
-
-//////////////////////////////////////
-
-var usb = new UsbAdapter();
-
-var webview = document.getElementById("sim-webview");
-
-var urlTextInput = document.getElementById("url-bar");
-var urlSubmitButton = document.getElementById("url-submit");
-
-var connectButton = document.getElementById("connect");
-var disconnectButton = document.getElementById("disconnect");
-var toggleFullscreenButton = document.getElementById("toggle-fullscreen");
-
-
-connectButton.addEventListener('click', viewerConnect);
-disconnectButton.addEventListener('click', viewerDisconnect);
-toggleFullscreenButton.addEventListener('click', viewerToggleFullscreen);
-
-
-
-
-
-
-
-var submitUrl = function(newUrl) {
-
-	if (!newUrl.match(/^[a-zA-Z]+:\/\//))
-	{
-	    newUrl = 'http://' + newUrl;
-	}
-
-	console.log("going to url=" + newUrl);
-	webview.src = newUrl;
-};
-
-urlSubmitButton.addEventListener('click', submitUrl);
-urlTextInput.addEventListener('keyup', function(event){
-	if (event.keyCode == 13) {
-		// enter was pressed
-		submitUrl(urlTextInput.value);
-	}
-});
-
-
-
-
-
-
-
-
-
-function viewerToggleFullscreen() {
-	console.log("toggling fullscreen");
-
-	var appWindow = chrome.app.window.current();
-
-	if (appWindow.isFullscreen()) {
-		appWindow.restore();
-	} else {
-		appWindow.fullscreen();
-	}
-}
-
-
-
-
-function viewerConnect() {
-	console.log("connect button pressed");
-	chrome.permissions.request( usb.getPermissionObject(), function(result) {
-    if (result) {
-    	console.log("got permission");
-      usb.connect();
-    } else {
-      console.log('App was not granted the "usbDevices" permission.');
-      console.log(chrome.runtime.lastError);
-    }
-  });
-}
-
-
-function viewerDisconnect() {
-	console.log("disconnect button pressed");
-	usb.disconnect();
-}
-
-
-
-
-
-
-var statDiv = document.getElementById("stats");
-
-function updateQuatLabel(quat) {
-	statDiv.innerText = JSON.stringify(quat);
-}
-
-
-
-var sendOrientationToSimulation = function(quat) {
-	webview.contentWindow.postMessage({x: quat._x, y: quat._y, z: quat._z, w: quat._w}, '*');
-}
-
-
-//----------------------------
-
-// GUI
-
-var gui = new dat.GUI();
-
-var actionGuiFolder = gui.addFolder('Actions');
-
-var actionObj = {
-	connect: viewerConnect,
-	disconnect: viewerDisconnect,
-	toggleFullscreen: viewerToggleFullscreen,
-	url: "google.com"
-};
-
-actionGuiFolder.add(actionObj, 'connect').name("Connect to Rift");
-actionGuiFolder.add(actionObj, 'disconnect').name("Disconnect from Rift");
-actionGuiFolder.add(actionObj, 'toggleFullscreen').name("Toggle Fullscreen");
-actionGuiFolder.add(actionObj, 'url').name("URL").onFinishChange(function(newUrl) {
-	submitUrl(newUrl);
-});
-actionGuiFolder.open();
-
-var configGuiFolder = gui.addFolder('Config');
-
-configGuiFolder.add(usb, 'mPredictDt', 0, 0.1).name("Orientation prediction (sec)");
